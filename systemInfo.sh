2c7e329715c6388a8494d4da81286700ac15a8d5
Josh <joshsedgman@gmail.com>
2024-10-11 12:32:28 -0400

    Made new changes to assignment1.sh file

--- systemInfo.sh
+++ systemInfo.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Display hostname
+# Display the hostname
 echo "Hostname: $(hostname)"
 
-# Display IP address
+# Display the IP address
 echo "IP Address: $(hostname -I | awk '{print $1}')"
 
-# Display Gateway IP address
+# Display the Gateway IP address
 echo "Gateway IP: $(ip route | grep default | awk '{print $3}')"
