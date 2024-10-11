f3a6848452453d578232a2b1782f668b3dc51da3
Josh <joshsedgman@gmail.com>
2024-10-11 12:59:21 -0400

    Made some new changes to my system report

--- assignment1.sh
+++ assignment1.sh
@@ -2,8 +2,8 @@
 
 # this script is intended to provide some system information
 
-USERNAME=$(whoami)
-DATETIME=$(date)
+username=$(whoami)
+datetime=$(date)
 
 # System Information
 
@@ -13,9 +13,16 @@ Uptime=$(uptime)
 
 #Hardware Information
 cpu_info=$(lscpu | grep 'Model name' | awk -F: '{print $2}' | xargs)
-CURRENT_CPU_SPEED=$(lscpu | grep 'CPU MHz' | awk '{print $3}') MHz
-MAX_CPU_SPEED=$(lscpu | grep 'CPU max MHz' | awk '{print $4}') MHz
-TOTAL_RAM=$(free -h | grep 'Mem:' | awk '{print $2}')
-DISK_INFO=$(lsblk -d -o NAME,SIZE,MODEL)
-VIDEO_CARD$(lspci | grep -i vga)
+current_cpu_speed=$(lscpu | grep 'CPU MHz' | awk '{print $3}') MHz
+maximum_cpu_speed=$(lscpu | grep 'CPU max MHz' | awk '{print $4}') MHz
+total_ram=$(free -h | grep 'Mem:' | awk '{print $2}')
+disk_info=$(lsblk -d -o NAME,SIZE,MODEL)
+video_card$(lspci | grep -i vga)
 
+#network Information
+
+fqdn=$(hostname -A)
+host_ip_address=$(hostname -I)
+gateway_ip=$(ip route | grep default)
+dns_server=$(cat /etc/resolv.conf)
+network_card_info=$(lspci | grep -i ethernet)
