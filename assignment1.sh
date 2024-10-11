3b0a43d7278d67187acac7e28009feca42b55794
Josh <joshsedgman@gmail.com>
2024-10-11 16:18:25 -0400

    Added some more lines to my report 4:15 Pm on friday october 11

--- assignment1.sh
+++ assignment1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # this script is intended to provide some system information
-
+# I need to add the echo statements below later
 username=$(whoami)
 datetime=$(date)
 
@@ -17,7 +17,7 @@ current_cpu_speed=$(lscpu | grep 'CPU MHz' | awk '{print $3}') MHz
 maximum_cpu_speed=$(lscpu | grep 'CPU max MHz' | awk '{print $4}') MHz
 total_ram=$(free -h | grep 'Mem:' | awk '{print $2}')
 disk_info=$(lsblk -d -o NAME,SIZE,MODEL)
-video_card$(lspci | grep -i vga)
+video_card=$(lspci | grep -i vga)
 
 #network Information
 
@@ -26,3 +26,14 @@ host_ip_address=$(hostname -I)
 gateway_ip=$(ip route | grep default)
 dns_server=$(cat /etc/resolv.conf)
 network_card_info=$(lspci | grep -i ethernet)
+ip_address_in_CIDR_format=$(hostname -I | awk '{print $1 "/24"}')
+
+#System status
+
+users_logged_in=$(who | awk '{print $1}' | sort -u | paste -sd ',')
+disk_space=$(df -h | awk 'NR>1 {print $6 ": " $4}')
+process_count=$(pgrep -c .)
+load_averages=$(cat /proc/loadavg | awk '{print $1 "," $2 "," $3}')
+memory_allocation=$(free -h | awk '/Mem:/ {print "Memory Allocation: Total: " $2 ", Used: " $3 ", Free: " $4}')
+listening_network_ports=$(ss -tuln)
+ufw_rules=$(ufw status)
